9d9dadc3ed962979981e6d242d790ea2bd297dbe
mshan0181 <103882696+mshan0181@users.noreply.github.com>
2023-12-26 18:55:35 +0530

    Commit on 26th Dec

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
- * New commit comment message 5
+ * New commit comment message on 26th DEC 
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello, World testing from  GITHUB");
   }
 }

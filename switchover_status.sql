fa281a644baf472f93d1bccbf41afc1554db1136
mshan0181 <103882696+mshan0181@users.noreply.github.com>
2024-01-10 00:35:23 +0530

    Update switchover_status.sql

--- switchover_status.sql
+++ switchover_status.sql
@@ -1,3 +1,26 @@
+set feed off
+set lines 120 pages 1000
+col tablespace_name format a20
+col allocated format 9999990.99
+col freespace format 9999990.99
+col maxsize format 9999990.99
+col pctused format 90.99
 spool swichover_logfile.log
-select switchover_status from v$database ;
+select tablespace_name, allocated, freespace,
+  (allocated-freespace)/maxsize*100 as pctused, maxsize
+  from
+  (
+    select a.tablespace_name,
+      sum(a.bytes)/1024/1024 as allocated,
+      (
+        select nvl(sum(b.bytes)/1024/1024,0)
+        from dba_free_space b
+        where b.tablespace_name = a.tablespace_name
+      ) as freespace,
+      sum(decode(a.maxbytes,0,a.bytes,a.maxbytes))/1024/1024 as maxsize
+    from dba_data_files a
+    group by a.tablespace_name
+  )
+where (allocated-freespace)/maxsize*100 > 90
+  order by 1;
 spool off 

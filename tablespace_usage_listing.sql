a4224749ac6e941a3cee118b1f9e6d249f993177
mshan0181 <mshan011181@gmail.com>
2024-01-11 08:44:59 +0530

    Update tablespace_usage_listing.sql

--- tablespace_usage_listing.sql
+++ tablespace_usage_listing.sql
@@ -15,7 +15,22 @@ col pctused format 90.99
 set pages 500
 SET MARKUP HTML ON  
 spool tablespace_usage_logfile.html
-
+SELECT df.tablespace_name "Tablespace",
+totalusedspace "Used MB",
+(df.totalspace - tu.totalusedspace) "Free MB",
+df.totalspace "Total MB",
+round(100 * ((df.totalspace - tu.totalusedspace) / df.totalspace)) "Pct. Free"
+FROM (SELECT tablespace_name, round(SUM(bytes) / 1048576) TotalSpace
+FROM dba_data_files
+GROUP BY tablespace_name) df,
+(SELECT round(SUM(bytes) / (1024 * 1024)) totalusedspace,
+tablespace_name
+FROM dba_segments
+GROUP BY tablespace_name) tu
+WHERE df.tablespace_name = tu.tablespace_name
+AND round(100 * ((df.totalspace - tu.totalusedspace) / df.totalspace)) < 80;
+spool off 
+spool tablespace_usage_logfile.log
 SELECT df.tablespace_name "Tablespace",
 totalusedspace "Used MB",
 (df.totalspace - tu.totalusedspace) "Free MB",

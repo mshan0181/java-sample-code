50ba56175e6f6ff2608c683fd4c88ac2b5d76334
mshan0181 <mshan011181@gmail.com>
2024-01-18 18:45:21 +0530

    Update expdp_export.sql

--- expdp_export.sql
+++ expdp_export.sql
@@ -1,4 +1,4 @@
 spool EXPDP_export.log
---host expdp hr/hr@192.168.101.237/testdb DIRECTORY=DATA_PUMP_DIR DUMPFILE=exp_schema_HR.dmp LOGFILE=HR_schema_EXP_LOG.log
-host expdp 192.168.101.237/testdb DIRECTORY=DATA_PUMP_DIR DUMPFILE=exp_schema_HR.dmp LOGFILE=HR_schema_EXP_LOG.log
+host expdp hr/hr@192.168.101.237/testdb DIRECTORY=DATA_PUMP_DIR DUMPFILE=exp_schema_HR.dmp LOGFILE=HR_schema_EXP_LOG.log
+---host expdp 192.168.101.237/testdb DIRECTORY=DATA_PUMP_DIR DUMPFILE=exp_schema_HR.dmp LOGFILE=HR_schema_EXP_LOG.log
 spool off 

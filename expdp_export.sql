227ccc31970a5babca4816fd82ae705e7125b070
mshan0181 <mshan011181@gmail.com>
2024-01-18 19:03:35 +0530

    Update expdp_export.sql

--- expdp_export.sql
+++ expdp_export.sql
@@ -3,3 +3,9 @@ host expdp hr/hr@192.168.101.237/testdb DIRECTORY=DATA_PUMP_DIR DUMPFILE=exp_sch
 ---host expdp 192.168.101.237/testdb DIRECTORY=DATA_PUMP_DIR DUMPFILE=exp_schema_HR.dmp LOGFILE=HR_schema_EXP_LOG.log
  step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'file', script: 'expdp_export.sql',scriptContent: '']) 
 spool off 
+
+
+bat 'type EXPDP_export.log'
+bat 'type EXPDP_export.log'
+bat 'type EXPDP_export.log'
+

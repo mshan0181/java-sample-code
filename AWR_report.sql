f23d224137646065dcbfca8d5b719e92dc79ae59
mshan0181 <mshan011181@gmail.com>
2024-01-19 09:47:39 +0530

    Update AWR_report.sql

--- AWR_report.sql
+++ AWR_report.sql
@@ -3,10 +3,6 @@ Set trimspool off
 Set linesize 1500
 Set termout on
 Set feedback off
-define DB_ID = '%dbid%'
-define INSTANCE_NO='%inst_num%'
-define BEGIN_ID='%bid%'
-define END_ID='%eid%'
 Spool awr_from_console.htm
 select output from table(dbms_workload_repository.awr_report_html(&DB_ID, &INSTANCE_NO, &BEGIN_ID, &END_ID));
 spool off;

de95a822fef697922e9fe42cc012b1b15ce1c014
mshan0181 <mshan011181@gmail.com>
2024-01-19 08:28:06 +0530

    Update AWR_report.sql

--- AWR_report.sql
+++ AWR_report.sql
@@ -8,6 +8,5 @@ define INSTANCE_NO='%inst_num%'
 define BEGIN_ID='%bid%'
 define END_ID='%eid%'
 Spool awr_from_console.htm
-select output from table(dbms_workload_repository.awr_report_html(2940744064,1,150,151));
 select output from table(dbms_workload_repository.awr_report_html(&DB_ID, &INSTANCE_NO, &BEGIN_ID, &END_ID));
 spool off;

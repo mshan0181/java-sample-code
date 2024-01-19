d79702227240e2d6f223e2c012973d66ad33ca0d
mshan0181 <mshan011181@gmail.com>
2024-01-19 07:39:39 +0530

    Update AWR_report.sql

--- AWR_report.sql
+++ AWR_report.sql
@@ -5,5 +5,5 @@ Set termout on
 Set feedback off
  
 Spool awr_from_console.htm
-select output from table(dbms_workload_repository.awr_report_html(%dbid%, %inst_num%, %bid%, %eid%));
+select output from table(dbms_workload_repository.awr_report_html('%dbid%',' %inst_num%', '%bid%', '%eid%'));
 spool off;

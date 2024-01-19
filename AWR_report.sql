7b9189388b20ac3b5d150166c7a2f053d4f03f6d
mshan0181 <mshan011181@gmail.com>
2024-01-19 07:17:56 +0530

    Create AWR_report.sql

---
+++ AWR_report.sql
@@ -0,0 +1,9 @@
+Set heading off
+Set trimspool off
+Set linesize 1500
+Set termout on
+Set feedback off
+ 
+Spool awr_from_console.htm
+select output from table(dbms_workload_repository.awr_report_html(&%dbid%, &%inst_num%, &%bid%, &%eid%));
+spool off;

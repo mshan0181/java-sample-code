6a6aa5899057b9b59cbec2e144f928f4e749a894
mshan0181 <mshan011181@gmail.com>
2024-01-20 22:15:20 +0530

    Update AWR_report.sql

--- AWR_report.sql
+++ AWR_report.sql
@@ -3,6 +3,6 @@ Set trimspool off
 Set linesize 1500
 Set termout on
 Set feedback off
-Spool awr_from_console.htm
+Spool awr_from_console.html
 select output from table(dbms_workload_repository.awr_report_html(&DB_ID, &INSTANCE_NO, &BEGIN_ID, &END_ID));
 spool off;

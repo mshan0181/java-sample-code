3a8be160412606df5c2fcab9a0f4f50dec47fd79
mshan0181 <mshan011181@gmail.com>
2024-01-28 07:32:10 +0530

    Update DBID_SNAPID_info.sql

--- DBID_SNAPID_info.sql
+++ DBID_SNAPID_info.sql
@@ -3,11 +3,11 @@ spool DB_id_info.log
 select DBID from v$database;
 spool off
 spool INST_no_info.log
-SELECT  INSTANCE_NUMBER  from dba_hist_snapshot where begin_interval_time > sysdate-1 and begin_interval_time like '%06.%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
+SELECT  INSTANCE_NUMBER  from dba_hist_snapshot where begin_interval_time > sysdate-2 and begin_interval_time like '%05.%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
 spool off  
 spool snapid_BEGIN_id.log
-SELECT  snap_id  from dba_hist_snapshot where begin_interval_time > sysdate-1 and begin_interval_time like '%05.%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
+SELECT  snap_id  from dba_hist_snapshot where begin_interval_time > sysdate-2 and begin_interval_time like '%05.%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
 spool off 
 spool snapid_END_id.log
-SELECT  snap_id  from dba_hist_snapshot where begin_interval_time > sysdate-1 and begin_interval_time like '%06.%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
+SELECT  snap_id  from dba_hist_snapshot where begin_interval_time > sysdate-2 and begin_interval_time like '%06.%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
 spool off

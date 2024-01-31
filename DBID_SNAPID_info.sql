2f12fa540cfc5e29ab5bf7eab4b186a7a7560e13
mshan0181 <mshan011181@gmail.com>
2024-01-31 15:38:25 +0530

    Update DBID_SNAPID_info.sql

--- DBID_SNAPID_info.sql
+++ DBID_SNAPID_info.sql
@@ -3,11 +3,11 @@ spool DB_id_info.log
 select DBID from v$database;
 spool off
 spool INST_no_info.log
-SELECT  INSTANCE_NUMBER  from  dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%' order by end_interval_time desc FETCH FIRST 1 ROWS ONLY; 
+select INSTANCE_NUMBER from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%06%';
 spool off 
 spool snapid_BEGIN_id.log
-select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%';
+ select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%06%';
 spool off 
 spool snapid_END_id.log
-select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%07%';
+select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%07%';
 spool off

21e4fef2c6d749224045a8bf7ab308070952184c
mshan0181 <mshan011181@gmail.com>
2024-01-31 13:34:27 +0530

    Update DBID_SNAPID_info.sql

--- DBID_SNAPID_info.sql
+++ DBID_SNAPID_info.sql
@@ -6,7 +6,7 @@ spool INST_no_info.log
 SELECT  INSTANCE_NUMBER  from  dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%' order by end_interval_time desc FETCH FIRST 1 ROWS ONLY; 
 spool off 
 spool snapid_BEGIN_id.log
-select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%'
+select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%';
 spool off 
 spool snapid_END_id.log
 select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%07%';

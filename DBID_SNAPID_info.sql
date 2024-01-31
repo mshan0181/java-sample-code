94d92cd89cf0a6179af899ad8651dc2b5719842f
mshan0181 <mshan011181@gmail.com>
2024-01-31 13:31:07 +0530

    Update DBID_SNAPID_info.sql

--- DBID_SNAPID_info.sql
+++ DBID_SNAPID_info.sql
@@ -3,11 +3,11 @@ spool DB_id_info.log
 select DBID from v$database;
 spool off
 spool INST_no_info.log
-SELECT  INSTANCE_NUMBER  from dba_hist_snapshot where begin_interval_time > sysdate-1 and begin_interval_time like '%06.30%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
-spool off  
+SELECT  INSTANCE_NUMBER  from  dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%' order by end_interval_time desc FETCH FIRST 1 ROWS ONLY; 
+spool off 
 spool snapid_BEGIN_id.log
-SELECT  snap_id  from dba_hist_snapshot where begin_interval_time > sysdate-1 and begin_interval_time like '%06.30%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
+select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%06%'
 spool off 
 spool snapid_END_id.log
-SELECT  snap_id  from dba_hist_snapshot where end_interval_time > sysdate-1 and end_interval_time like '%08.30%' order by begin_interval_time desc FETCH FIRST 1 ROWS ONLY;
+select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and end_interval_time like '%07%';
 spool off

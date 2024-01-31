606e80ab4bdb3413f57b51078146732c932f1194
mshan0181 <mshan011181@gmail.com>
2024-01-31 15:38:39 +0530

    Update DBID_SNAPID_info.sql

--- DBID_SNAPID_info.sql
+++ DBID_SNAPID_info.sql
@@ -6,7 +6,7 @@ spool INST_no_info.log
 select INSTANCE_NUMBER from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%06%';
 spool off 
 spool snapid_BEGIN_id.log
- select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%06%';
+select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%06%';
 spool off 
 spool snapid_END_id.log
 select snap_id from    dba_hist_snapshot where  to_char(begin_interval_time,'dd/mm/yy')=to_char(sysdate,'dd/mm/yy') and SUBSTR(end_interval_time, 11, 2) like '%07%';

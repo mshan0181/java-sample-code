d2d5d42bb1072ea89de565e4b6d9c970e7ca3a7b
mshan0181 <mshan011181@gmail.com>
2024-03-23 18:06:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println('Hello,World2222222');
+    System.out.println("Hello, World testing from  GITHUB new ");
   }
 }
